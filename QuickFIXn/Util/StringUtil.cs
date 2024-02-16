ab4e6f19c7e967f639b3f78f251651f95ec953af
Shahar Kahn <shahar.ygm@gmail.com>
2024-02-16 12:51:29 +0200

    minor

--- QuickFIXn/Util/StringUtil.cs
+++ QuickFIXn/Util/StringUtil.cs
@@ -1,12 +1,10 @@
-using System;
 using System.Runtime.InteropServices;
-using QuickFix.Fields;
 
 namespace QuickFix.Util
 {
     public static class StringUtil
     {
-        static public string FixSlashes(string s)
+        public static string FixSlashes(string s)
         {
             if(RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
                 return s.Replace('/', '\\');
